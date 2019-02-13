fc836cc3db56302607aebe40856e58a0408c493e
Keith Mannock <keith@dcs.bbk.ac.uk>
2019-02-13 11:50:15 +0000

    Added "Bridge" design pattern example

---
+++ designpatterns/src/bridge/Colour.java
@@ -0,0 +1,5 @@
+package bridge;
+
+public interface Colour {
+    void applyColour();
+}
---
+++ designpatterns/src/bridge/RedColour.java
@@ -0,0 +1,8 @@
+package bridge;
+
+public class RedColour implements Colour {
+    @Override
+    public void applyColour() {
+        System.out.println("red.");
+    }
+}
